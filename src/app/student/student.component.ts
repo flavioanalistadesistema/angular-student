b4957417dd1e2ec50f85d2f9a5705dc0edbf56b3
MrRobotProgrammer <flavio.santos@pravaler.com.br>
2020-08-31 18:28:40 -0300

    Aplicando Property Binding nos Primeiros Componentes

---
+++ src/app/student/student.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css']
+})
+export class StudentComponent implements OnInit {
+  name: string = 'Flávio';
+  isProgrammer: boolean = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
