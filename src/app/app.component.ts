6650078d141a2fe73eb81096069bd929eb2fd847
MrRobotProgrammer <flavio.santos@pravaler.com.br>
2020-09-01 09:38:23 -0300

    Incluindo novas opções para DOM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularUdemy';
+  flavio   = {name:'Flávio', isProgrammer: true, linguage: 'PHP'}
+  henrique = {name:'Henrique', isProgrammer: false }
+  patricia = {name:'Patricia', isProgrammer: true, linguage: 'JavaScript'}
 }
